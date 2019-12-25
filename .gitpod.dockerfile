c12173b1b32a9307f4fba3925d30a722c0221ac7
Masahiro Yoshida <mypianoplayer@gmail.com>
2019-12-25 12:00:37 +0000

    environment setting and port example source

---
+++ .gitpod.dockerfile
@@ -0,0 +1,6 @@
+FROM gitpod/workspace-full:latest
+
+RUN sudo apt-get update \
+ && sudo apt-get install -y \
+    libudev-dev zlib1g-dev alsa libasound2-dev \
+ && sudo rm -rf /var/lib/apt/lists/*
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+// Draw some multi-colored geometry to the screen
+extern crate quicksilver;
+
+use quicksilver::{
+    Result,
+    geom::{Circle, Line, Rectangle, Transform, Triangle, Vector},
+    graphics::{Background::Col, Color},
+    lifecycle::{Settings, State, Window, run},
+};
+
+struct DrawGeometry;
+
+impl State for DrawGeometry {
+    fn new() -> Result<DrawGeometry> {
+        Ok(DrawGeometry)
+    }
+
+    fn draw(&mut self, window: &mut Window) -> Result<()> {
+        window.clear(Color::WHITE)?;
+        window.draw(&Rectangle::new((100, 100), (32, 32)), Col(Color::BLUE));
+        window.draw_ex(&Rectangle::new((400, 300), (32, 32)), Col(Color::BLUE), Transform::rotate(45), 10);
+        window.draw(&Circle::new((400, 300), 100), Col(Color::GREEN));
+        window.draw_ex(
+            &Line::new((50, 80),(600, 450)).with_thickness(2.0),
+            Col(Color::RED),
+            Transform::IDENTITY,
+            5
+        );
+        window.draw_ex(
+            &Triangle::new((500, 50), (450, 100), (650, 150)),
+            Col(Color::RED),
+            Transform::rotate(45) * Transform::scale((0.5, 0.5)),
+            0
+        );
+        Ok(())
+    }
+}
+
+fn main() {
+    run::<DrawGeometry>("Draw Geometry", Vector::new(800, 600), Settings::default());
+}
